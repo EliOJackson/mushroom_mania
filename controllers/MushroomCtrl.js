451383094f8a316a6c707ec39e947f42abfa4cdf
Eli Jackson <tucker.eli.jackson@gmail.com>
2018-01-26 10:24:45 -0600

    took out console logs, and commented on partial code

--- controllers/MushroomCtrl.js
+++ controllers/MushroomCtrl.js
@@ -1,10 +1,8 @@
 'use strict';
 
 angular.module("mushroomFun").controller("MushroomCtrl", function($scope, MushroomFactory) {
-    console.log('hello')
     MushroomFactory.getMushrooms()
     .then( (mushroomData) => {
         $scope.allShrooms = Object.values(mushroomData.data)
-        console.log('all shrooms', $scope.allShrooms);
     })
 })
